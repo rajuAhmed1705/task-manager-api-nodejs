ce4aff4d0a44de359f976a59c16c24cf9346a8f8
rajuAhmed1705 <43791325+rajuAhmed1705@users.noreply.github.com>
2020-05-28 22:25:44 +0600

    init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+    useNewUrlParser:true,
+    useCreateIndex:true
+})
+
+
+
+
+
+
+// const task = new Task({
+//     description:'complete this node course',
+//     completed: true
+// })
+
+// task.save().then(() => {
+//     console.log(task);
+    
+// }).catch((error) => {
+//     console.log(error);
+    
+// })
